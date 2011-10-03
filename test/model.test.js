5500e1c7b65a3e4ef859f58a866680173f35f50a
Jake Luer <jake.luer@gmail.com>
2011-10-03 08:34:01 -0400

    refactored as model/collection factory

---
+++ test/model.test.js
@@ -0,0 +1,39 @@
+var assert = require('assert'),
+    seed = require('seed');
+
+module.exports = {
+  'version exists': function () {
+    assert.isNotNull(seed.version);
+  },
+  'model creation': function () {
+    var person = seed.model.extend();
+    
+    var jake = new person({
+      name: 'jake'
+    });
+    
+    assert.type(jake, 'object', 'model of correct type');
+    assert.eql(jake.attributes, { name: 'jake' }, 'model has correct attributes');
+    assert.equal(true, jake instanceof person, 'model is correct instance');
+  },
+  'models have events': function () {
+    var person = seed.model.extend(),
+        n = 0;
+    
+    var jake = new person({
+      name: 'jake'
+    });
+    
+    jake.on('testing', function() {
+      n++;
+    });
+    
+    setTimeout(function() {
+      jake.emit('testing');
+    }, 200);
+    
+    this.on('exit', function () {
+      assert.equal(n, 1, 'event has successfully executed callback');
+    });
+  }
+};
---
+++ test/utils.test.js
@@ -0,0 +1,26 @@
+var assert = require('assert'),
+    seed = require('seed');
+
+module.exports = {
+  'version exists': function() {
+    assert.isNotNull(seed.version);
+  },
+  
+  'basic object extends': function() {
+    var obj1 = {
+      'doctor': 'who',
+      'companion': 'rose',
+      'tardis': {
+        time: 'relative dimension'
+      }
+    };
+    
+    var obj2 = {
+      'companion': 'amy',
+      'tardis': 'police box'
+    };
+    
+    var obj3 = seed.extend(obj1, obj2);
+    assert.eql(obj3, { doctor: 'who', companion: 'amy', tardis: 'police box' });
+  }
+};
