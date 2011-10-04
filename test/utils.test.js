1de767cf47c8f52f91989782322fbd62efb4ae90
Jake Luer <jake.luer@gmail.com>
2011-10-04 10:37:59 -0400

    model is actually Model (caps)

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,6 @@
 var assert = require('assert'),
-    seed = require('seed');
+    seed = require('seed'),
+    utils = require('utils');
 
 module.exports = {
   'version exists': function() {
@@ -20,7 +21,7 @@ module.exports = {
       tardis: 'police box'
     };
     
-    var obj3 = seed.merge(obj1, obj2);
+    var obj3 = utils.merge(obj1, obj2);
     assert.eql(obj3, { doctor: 'who', companion: 'amy', tardis: 'police box' });
   }
 };
