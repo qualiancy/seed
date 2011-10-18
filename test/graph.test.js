16d5b0e82c2f438a570f09269ab022a37099c5ed
Jake Luer <jake.luer@gmail.com>
2011-10-18 02:31:05 -0400

    basic use model as schema

--- test/graph.test.js
+++ test/graph.test.js
@@ -22,5 +22,36 @@ module.exports = {
     this.on('exit', function () {
       assert.equal(1,n, 'all callbacks have been called');
     });
+  },
+  'graph stores simple models': function () {
+    var n = 0;
+    
+    var person = Seed.Model.extend({
+      type: 'person',
+      schema: {
+        name: {
+          type: String,
+          unique: true
+        },
+        origin: String
+      }
+    });
+    
+    var earth = new Seed.Graph({
+      models: [ person ]
+    });
+    
+    earth.add('person', {
+      name: 'Arthur Dent',
+      origin: 'Earth'
+    });
+    
+    earth.add('person', {
+      name: 'Ford Prefect',
+      origin: 'Betelgeuse-ish'
+    });
+    
+    assert.equal(2, earth.length(), 'Both items added to graph.');
+    
   }
 };
