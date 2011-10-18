2b524440a6797f7358d638ffed7cde9a27d4f5a3
Jake Luer <jake.luer@gmail.com>
2011-10-18 01:55:09 -0400

    graph initialized correctly

--- test/graph.test.js
+++ test/graph.test.js
@@ -7,5 +7,20 @@ var sherlock = require('sherlock');
 module.exports = {
   'seed#version': function () {
     assert.isNotNull(Seed.version);
+  },
+  'graph#constructor': function () {
+    var n = 0;
+    
+    var graph = Seed.Graph.extend({
+      initialize: function () {
+        n++;
+      }
+    });
+    
+    var g = new graph();
+    
+    this.on('exit', function () {
+      assert.equal(1,n, 'all callbacks have been called');
+    });
   }
 };
