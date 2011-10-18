74107283ce733b59d6f5c8efd544d2fa8ef1ebcc
Jake Luer <jake.luer@gmail.com>
2011-10-18 03:26:19 -0400

    can add models to graphs

--- test/graph.test.js
+++ test/graph.test.js
@@ -1,8 +1,10 @@
 
 var assert = require('assert'),
+    tea = require('tea'),
     Seed = require('..');
 
-var sherlock = require('sherlock');
+var sherlock = require('sherlock')
+  , inspect = require('eyes').inspector({ maxLength: 10240 });
 
 module.exports = {
   'seed#version': function () {
@@ -23,11 +25,10 @@ module.exports = {
       assert.equal(1,n, 'all callbacks have been called');
     });
   },
-  'graph stores simple models': function () {
+  'graph stores simple models, emits events': function () {
     var n = 0;
     
-    var person = Seed.Model.extend({
-      type: 'person',
+    var person = Seed.Model.extend('person', {
       schema: {
         name: {
           type: String,
@@ -41,17 +42,49 @@ module.exports = {
       models: [ person ]
     });
     
-    earth.add('person', {
+    var arthur = {
+      id: 'arthur',
       name: 'Arthur Dent',
       origin: 'Earth'
-    });
+    };
     
-    earth.add('person', {
+    var ford = {
+      id: 'ford',
       name: 'Ford Prefect',
       origin: 'Betelgeuse-ish'
+    };
+    
+    var success = function (err, data) {
+      n++;
+      assert.isNull(err);
+      assert.isNotNull(data);
+    };
+    
+    var fail = function (err, data) {
+      n++;
+      assert.isNotNull(err);
+      assert.isNull(data);
+    }
+    
+    earth.on('new:person', function (person) {
+      n++;
+      assert.isNotNull(person);
+      assert.ok(tea.isType(person, 'person'));
     });
     
-    assert.equal(2, earth.length(), 'Both items added to graph.');
+    earth.add('person', arthur, success);
+    earth.add('person', ford, success);
+    
+    earth.add('alien', ford, fail);
+    
+    inspect(earth);
+    
+    
+    
+    this.on('exit', function () {
+      assert.equal(2, earth.length(), 'Both items added to graph.');
+      assert.equal(5, n, 'All callbacks fired');
+    });
     
   }
 };
