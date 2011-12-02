1ed2b9e7261e86de9dde3ee7107388a9f6dae8cf
Jake Luer <jake.luer@gmail.com>
2011-12-02 02:26:26 -0500

    schema example comments

--- examples/schema.js
+++ examples/schema.js
@@ -1,10 +1,10 @@
 var Seed = require('..')
   , Type = Seed.Schema.Type;
 
-var States = new Type.Select(['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI',
-'ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO',
-'MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI',
-'SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']);
+var States = new Type.Select(['AL','AK','AZ','AR','CA','CO','CT','DE',
+'FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI',
+'MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK',
+'OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']);
 
 var Person = new Seed.Schema({
   name: {
@@ -49,5 +49,5 @@ var traveller_2 = {
 var valid_person = Person.validate(traveller_1);
 var invalid_person = Person.validate(traveller_2);
   
-console.log(traveller_1.name + ' valid: ' + valid_person);
-console.log(traveller_2.name + ' valid: ' + invalid_person);
+console.log(traveller_1.name + ' valid: ' + valid_person); // true
+console.log(traveller_2.name + ' valid: ' + invalid_person); // false
