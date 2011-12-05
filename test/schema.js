5e8817af3e88a5f6fe50ef5942bdb519fb56dfa3
Jake Luer <jake.luer@gmail.com>
2011-12-04 22:38:18 -0500

    schema tests for required and nested data

--- test/schema.js
+++ test/schema.js
@@ -9,4 +9,58 @@ describe('Schema', function () {
     should.exist(Seed.version);
   });
 
+  describe('required data', function () {
+    var s = new Seed.Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: Number
+    });
+
+    it('should validate when required field included', function () {
+      s.validate({ name: 'Seed' }).should.be.ok;
+    });
+
+    it('should not validate when required field is missing', function () {
+      s.validate({ age: 1 }).should.not.be.ok;
+    });
+  });
+
+  describe('nested data', function () {
+    var s = new Seed.Schema({
+      name: {
+        first: {
+          type: String,
+          required: true
+        },
+        last: String
+      }
+    });
+
+    it('should validate when required fields included', function () {
+      var p = {
+        name: {
+          first: 'Node',
+          last: 'js'
+        }};
+
+      s.validate(p).should.be.ok;
+    });
+
+    it('should not validate if data for parent not object', function () {
+      var p = { name: 'Node.js' };
+
+      s.validate(p).should.not.be.ok;
+    });
+
+    it('should not validate when required field is missing', function () {
+      var p = {
+        name: {
+          last: 'js'
+        }};
+
+      s.validate(p).should.not.be.ok;
+    });
+  });
 });
