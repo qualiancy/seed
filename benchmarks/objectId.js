0fc2283b430e689a0280e797f644798564fca301
Jake Luer <jake.luer@gmail.com>
2011-12-06 07:14:14 -0500

    benchmarks `matcha` 0.0.2 compatible

--- benchmarks/objectId.js
+++ benchmarks/objectId.js
@@ -1,14 +1,7 @@
-var matcha = require('matcha')
-  , Seed = require('../lib/seed')
+var Seed = require('../lib/seed')
   , ObjectId = new Seed.ObjectId();
 
-var suite = new matcha.Suite({
-  iterations: 10000,
-});
-
-suite.bench('generate', function (next) {
+bench('generate', function (next) {
   var _id = ObjectId.gen();
   next();
 });
-
-module.exports = suite;
