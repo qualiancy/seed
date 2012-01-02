eda86d4127ba976ba7b5382cb0dea033edd1c4ca
Jake Luer <jake.luer@gmail.com>
2012-01-02 03:32:20 -0500

    tests completed for model crud operations for memory store

--- test/store.memory.js
+++ test/store.memory.js
@@ -19,12 +19,78 @@ describe('MemoryStore', function () {
 
   describe('CRUD from Model', function () {
 
+    // store uses hash so we can compare expectations.
+    var store = new MemoryStore();
+
     var Person = Seed.Model.extend('person', {
-        store: new MemoryStore()
+        store: store
+    });
+
+    var arthur = new Person({
+        id: 'arthur'
+      , name: 'Arthur Dent'
     });
 
     it('should allow a new object to be created', function (done) {
-      done();
+      arthur.save(function (err) {
+        should.not.exist(err);
+        store.store.length.should.equal(1);
+        arthur._attributes.should.eql(store.store.get('arthur'));
+        done();
+      });
+    });
+
+    it('should allow an already written object to be retrieved', function (done) {
+      var dent = new Person({
+          id: 'arthur'
+      });
+
+      dent.fetch(function (err) {
+        should.not.exist(err);
+        dent._attributes.should.eql(arthur._attributes);
+        done();
+      })
+    });
+
+    it('should allow an already written object to be modified', function (done) {
+
+      arthur.set({ 'location': 'earth' });
+      arthur.save(function (err) {
+        should.not.exist(err);
+
+        var confirm = new Person({
+            id: 'arthur'
+        });
+
+        confirm.fetch(function (err) {
+          should.not.exist(err);
+          confirm._attributes.should.eql({
+              id: 'arthur'
+            , name: 'Arthur Dent'
+            , location: 'earth'
+          });
+          done();
+        });
+      });
+    });
+
+    it('should allow an already existing item to be removed', function (done) {
+
+      arthur.destroy(function (err) {
+        should.not.exist(err);
+
+        var confirm = new Person({
+            id: 'arthur'
+        });
+
+        confirm.fetch(function (err) {
+          should.exist(err);
+          err.code.should.equal(3);
+          done();
+        });
+
+      });
+
     });
 
   });
