f1a9bd31644917b9a8eb7c961c1e343f1ea77fc8
Jake Luer <jake.luer@gmail.com>
2012-01-02 02:54:13 -0500

    memorystore constructor test

--- test/store.memory.js
+++ test/store.memory.js
@@ -8,4 +8,25 @@ describe('MemoryStore', function () {
     Seed.version.should.match(/^\d+\.\d+\.\d+$/);
   });
 
+  describe('constructor', function () {
+
+    it('should call initialize', function () {
+      var Store = new MemoryStore();
+      Store.store.should.be.an.instanceof(Seed.Hash);
+    });
+
+  });
+
+  describe('CRUD from Model', function () {
+
+    var Person = Seed.Model.extend('person', {
+        store: new MemoryStore()
+    });
+
+    it('should allow a new object to be created', function (done) {
+      done();
+    });
+
+  });
+
 });
