e073c8c119fe126d7511c0c00b6700c194bcba9c
Jake Luer <jake.luer@gmail.com>
2012-01-28 17:30:06 -0500

    few simple tests

--- test/model.js
+++ test/model.js
@@ -8,5 +8,15 @@ describe('Model', function () {
     Seed.version.should.match(/^\d+\.\d+\.\d+$/);
   });
 
+  describe('managing attributes', function () {
+    var model = new Seed.Model();
+    it('should allow for attributes to be `set`', function () {
+      model.set({ hello: 'universe' });
+      model._attributes.hello.should.equal('universe');
+    });
 
+    it('should allow for attributed to be `get`', function () {
+      model.get('hello').should.equal('universe');
+    });
+  });
 });
