282252d3c52d2c5e8474591f4104f07689fa8c77
Jake Luer <jake.luer@gmail.com>
2012-04-27 02:28:26 -0400

    changed flow util `waterfall` to `concurrent` as it inappropriately named

--- test/utils.flow.js
+++ test/utils.flow.js
@@ -8,6 +8,7 @@ describe('Flow Utils', function () {
   it('should do a series of async calls', function (done) {
     var arr = [ 1, 2, 3, 4, 5, 6 ]
       , count = 0;
+
     Seed.utils.series(arr, function (num, index, next) {
       index.should.be.equal(num - 1);
       setTimeout(function () {
@@ -23,10 +24,11 @@ describe('Flow Utils', function () {
 
   it('should do a parallel of async calls');
 
-  it('should do a waterfall of async calls', function (done) {
+  it('should do a concurrent execution of async calls', function (done) {
     var arr = [ 1, 2, 3, 4, 5, 6 ]
       , count = 0;
-    Seed.utils.waterfall(arr, 2, function (num, index, next) {
+
+    Seed.utils.concurrent(arr, 2, function (num, index, next) {
       count++;
       index.should.be.equal(num - 1);
       count.should.be.below(3);
