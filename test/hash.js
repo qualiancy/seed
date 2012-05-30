56bc11e59657fcbcec6a6d5be68252623ed9540f
Jake Luer <jake.luer@gmail.com>
2012-05-30 00:07:12 -0400

    tests for Hash#reduce and Hash#mapReduce

--- test/hash.js
+++ test/hash.js
@@ -1,4 +1,5 @@
-var should = require('chai').should();
+var chai = require('chai')
+  , should = chai.should();
 
 var Seed = require('..')
   , Hash = Seed.Hash;
@@ -204,6 +205,64 @@ describe('Hash', function () {
         n.should.equal(expected_length);
       });
 
+    });
+
+    describe('#reduce', function () {
+      var hash = new Hash(data)
+        , n = 0;
+
+      it('should be able to reduce', function () {
+        var res =hash.reduce(function (value, key) {
+          return 1;
+        });
+
+        res.should.equal(hash.length);
+      });
+
+      it('should be able to map reduce', function () {
+
+        var mapFn = function (key, value, emit) {
+          var firstLetter = key[0].toUpperCase();
+          emit(firstLetter, value);
+        }
+
+        var reduceFn = function (key, values) {
+          var res = 0;
+          values.forEach(function (v) {
+            res += v;
+          });
+          return res;
+        }
+
+        var result = hash.mapReduce(mapFn, reduceFn);
+        chai.expect(result._data).eql({
+           A: 164634460,
+           B: 499541913,
+           C: 1612921712,
+           D: 16332279,
+           E: 201923164,
+           F: 71801966,
+           G: 153667627,
+           H: 34962898,
+           I: 1616633286,
+           J: 135946476,
+           K: 139914902,
+           L: 29208715,
+           M: 265693642,
+           N: 235034558,
+           O: 3027959,
+           P: 387744743,
+           Q: 848016,
+           R: 172014868,
+           S: 286525125,
+           T: 243917984,
+           U: 492263162,
+           V: 118519363,
+           W: 3091113,
+           Y: 24133492,
+           Z: 25965640
+        });
+      });
     });
 
     describe('#find', function () {
